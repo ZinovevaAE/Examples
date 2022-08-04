ed6b62cc3a4320dc08c67e6bef316b01d2726456
AZinoveva <aytsaan@yandex.ru>
2022-08-04 12:24:40 +0300

    add Example 005 If Else

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mari")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
