95aed4d7a56def2f45e52972eceebb7ca4930a8e
AZinoveva <aytsaan@yandex.ru>
2022-08-06 14:00:15 +0300

    Add formating

--- Example_008_About_dog/Program.cs
+++ Example_008_About_dog/Program.cs
@@ -6,7 +6,7 @@ Console.Write("Введите скорость собаки: ");
 int dogSpeed = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите скорость первого человека (должна быть больше скорости второго): ");
-int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());  
+int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите скорость второго человека: ");
 int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
@@ -18,20 +18,20 @@ int count = 0;
 int friend = 2;
 int time = 0;
 
-while(distance>=10)
+while (distance >= 10)
 {
-    if(friend == 2)
+    if (friend == 2)
     {
-        time= distance/(dogSpeed-secondFriendSpeed);
-        friend=1;
+        time = distance / (dogSpeed - secondFriendSpeed);
+        friend = 1;
     }
     else
     {
-        time=distance/ (dogSpeed+firstFriendSpeed);
-        friend=2;
+        time = distance / (dogSpeed + firstFriendSpeed);
+        friend = 2;
     }
-    distance=distance-firstFriendSpeed*time+secondFriendSpeed*time;
-    count = count +1;
+    distance = distance - firstFriendSpeed * time + secondFriendSpeed * time;
+    count = count + 1;
 }
 
 Console.Write(count);
